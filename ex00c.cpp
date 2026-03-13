e594addfd2cf1fcc247ce55054778789615aad0a
Alex Luciano Roesler Rese <alexrese@outlook.com>
2026-03-12 21:55:11 -0300

    Create ex00c.cpp

---
+++ ex00c.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float valorCamiseta;
+    cout<<"Digite o valor real da camiseta: "<<endl;
+    cin>>valorCamiseta;
+    cout<<"Valor real da camiseta: "<<valorCamiseta<<endl;
+    cout<<"Valor a ser pago com desconto: " <<valorCamiseta*0.9;
+
+    return 0;
+}
