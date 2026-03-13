792a738ac8817e198454b3a6bd29b6521701e80f
Alex Luciano Roesler Rese <alexrese@outlook.com>
2026-03-12 21:54:33 -0300

    Create ex00b.cpp

---
+++ ex00b.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int valor;
+    cout<<"Digite um valor inteiro: " <<endl;
+    cin>>valor;
+    cout<<"Antecessor: "<< valor-1 << " Sucessor: " << valor+1;
+
+    return 0;
+}
