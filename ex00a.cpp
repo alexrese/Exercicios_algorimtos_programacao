6cd2a179ac8da0855ed612a8bd1a74bd2ca6694e
Alex Luciano Roesler Rese <alexrese@outlook.com>
2026-03-12 21:53:47 -0300

    Create ex00a.cpp

---
+++ ex00a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float valor1, valor2, media;
+    cout<<"Digite dois valores:"<<endl;
+    cin>>valor1>>valor2;
+    media=(valor1+valor2)/2;
+    cout<<"A media dos valores: "<<media;
+    
+
+    return 0;
+}
